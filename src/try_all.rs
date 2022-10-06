bc413be705cbced1d7921388a10b1d13211c4e7d
Arnau Abella <arnauabella@gmail.com>
2022-10-06 16:36:03 +0200

    Fix implementation of try_all

--- src/try_all.rs
+++ src/try_all.rs
@@ -1,6 +1,6 @@
 use futures::{
-    future::{IntoFuture, TryMaybeDone},
-    Future, TryFuture, TryFutureExt,
+    future::{Fuse, FusedFuture, IntoFuture},
+    Future, FutureExt, TryFuture, TryFutureExt,
 };
 use std::{
     mem,
@@ -8,10 +8,9 @@ use std::{
     task::{Context, Poll},
 };
 
-enum FinalState {
-    Pending,
-    AllError,
-    Done(usize),
+trait FusedTryFuture: FusedFuture<Output = Result<Self::Ok, Self::Err>> {
+    type Ok;
+    type Err;
 }
 
 #[must_use = "futures do nothing unless you `.await` or poll them"]
@@ -19,7 +18,20 @@ pub struct TryAll<F>
 where
     F: TryFuture,
 {
-    elems: Pin<Box<[TryMaybeDone<IntoFuture<F>>]>>,
+    elems: Pin<Box<[Fuse<IntoFuture<F>>]>>,
+}
+
+pub fn try_all<I>(iter: I) -> TryAll<I::Item>
+where
+    I: IntoIterator,
+    I::Item: TryFuture,
+{
+    let x = iter
+        .into_iter()
+        .map(TryFutureExt::into_future)
+        .map(|fut| fut.fuse())
+        .collect::<Box<[_]>>();
+    TryAll { elems: x.into() }
 }
 
 fn iter_pin_mut<T>(slice: Pin<&mut [T]>) -> impl Iterator<Item = Pin<&mut T>> {
@@ -28,15 +40,10 @@ fn iter_pin_mut<T>(slice: Pin<&mut [T]>) -> impl Iterator<Item = Pin<&mut T>> {
         .map(|t| unsafe { Pin::new_unchecked(t) })
 }
 
-pub fn try_all<I>(iter: I) -> TryAll<I::Item>
-where
-    I: IntoIterator,
-    I::Item: TryFuture,
-{
-    let iter = iter.into_iter().map(TryFutureExt::into_future);
-    TryAll {
-        elems: iter.map(TryMaybeDone::Future).collect::<Box<[_]>>().into(),
-    }
+enum FinalState<T> {
+    Pending,
+    Done(T),
+    AllError,
 }
 
 impl<F> Future for TryAll<F>
@@ -48,44 +55,33 @@ where
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let mut state = FinalState::AllError;
 
-        for (i, elem) in iter_pin_mut(self.elems.as_mut()).enumerate() {
-            let x = elem.try_poll(cx);
-            match x {
-                Poll::Pending => state = FinalState::Pending,
-                Poll::Ready(Ok(())) => {
-                    state = FinalState::Done(i);
-                    break;
-                }
-                Poll::Ready(Err(_)) => {}
-            }
-        }
-
-        match state {
-            FinalState::Pending => Poll::Pending,
-            FinalState::Done(idx) => {
-                let mut elems = mem::replace(&mut self.elems, Box::pin([]));
-                let mut r: Option<F::Ok> = None;
-                for (i, elem) in iter_pin_mut(elems.as_mut()).enumerate() {
-                    if i == idx {
-                        r = Some(elem.take_output().unwrap());
+        for elem in iter_pin_mut(self.elems.as_mut()) {
+            if !elem.is_terminated() {
+                let x = elem.try_poll(cx);
+                match x {
+                    Poll::Pending => state = FinalState::Pending,
+                    Poll::Ready(Ok(a)) => {
+                        state = FinalState::Done(a);
                         break;
                     }
+                    Poll::Ready(Err(_)) => {}
                 }
-                Poll::Ready(r)
-            }
-            FinalState::AllError => {
-                let _ = mem::replace(&mut self.elems, Box::pin([]));
-                Poll::Ready(None)
             }
         }
+        let output = match state {
+            FinalState::Pending => return Poll::Pending,
+            FinalState::Done(a) => Some(a),
+            FinalState::AllError => None,
+        };
+        let _ = mem::replace(&mut self.elems, Box::pin([]));
+        Poll::Ready(output)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::time::Duration;
-    use tokio::time::sleep;
+    use tokio::time;
 
     fn assert_future<T, F>(future: F) -> F
     where
@@ -94,12 +90,32 @@ mod tests {
         future
     }
 
-    #[tokio::test]
-    async fn test_try_all() {
+    #[tokio::test(flavor = "multi_thread", worker_threads = 8)]
+    async fn test_try_all_all_fail() {
+        let mut v = Vec::new();
+        for i in 1..=10 {
+            let fut = assert_future::<Result<usize, String>, _>(async move {
+                let mut interval = time::interval(time::Duration::from_millis(10));
+                for _ in i..=10 {
+                    interval.tick().await;
+                }
+                Err("Bad".to_string())
+            });
+            v.push(fut);
+        }
+        let r = try_all(v).await;
+        assert_eq!(r, None);
+    }
+
+    #[tokio::test(flavor = "multi_thread", worker_threads = 8)]
+    async fn test_try_all_once_success() {
         let mut v = Vec::new();
         for idx in 1..=10 {
             let fut = assert_future::<Result<usize, String>, _>(async move {
-                sleep(Duration::from_millis(100)).await;
+                let mut interval = time::interval(time::Duration::from_millis(10));
+                for _i in 0..5 {
+                    interval.tick().await;
+                }
                 if idx == 10 {
                     Ok(0)
                 } else {
